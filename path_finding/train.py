3aa985b096cd79dc8960f80db1f23fa0ec961cdf
angsylvest <sylve057@umn.edu>
2024-02-28 11:33:15 -0600

    fix to env input type

--- path_finding/train.py
+++ path_finding/train.py
@@ -12,10 +12,6 @@ def train(sequence_length):
     x_time_df_others = pd.read_csv('dataset/x_time_train_other_agents.csv')
     y_df = pd.read_csv('dataset/y_train_test.csv')
 
-    # Convert grid representations to nested lists
-    # x_time_df['env_input'] = x_time_df['env_input'].apply(lambda x: eval(x))
-    # x_time_df_others['env_input_others'] = x_time_df_others['env_input_others'].apply(lambda x: eval(x))
-
     # Convert grid representations to nested lists of integers
     x_time_df['env_input'] = x_time_df['env_input'].apply(lambda x: np.array(eval(x)))
     x_time_df_others['env_input_others'] = x_time_df_others['env_input_others'].apply(lambda x: np.array(eval(x)))
@@ -47,22 +43,14 @@ def train(sequence_length):
     # Concatenate the input arrays
     concatenated_input = np.concatenate((x_time_scaled, x_time_scaled_others), axis=1)
 
-    print(f'x_time_scaled {x_time_scaled} and x_time_scaled_others {x_time_scaled_others}')
+    # print(f'x_time_scaled {x_time_scaled} and x_time_scaled_others {x_time_scaled_others}')
     y_scaled = merged_df['action'].values.reshape(-1, 1)
 
     # Convert to PyTorch tensors
     concatenated_tensor = torch.tensor(concatenated_input, dtype=torch.float32)
-    # x_time_tensor = torch.tensor(x_time_scaled, dtype=torch.float32)
-    # x_time_others = torch.tensor(x_time_scaled_others, dtype=torch.float32)
-
-    # print(x_time_tensor.shape)
-    # print(x_time_others.shape)
-
-
     y_tensor = torch.tensor(y_scaled, dtype=torch.float32).squeeze().long()  # Squeeze to remove extra dimension
 
     # Reshape tensors for LSTM input
-    # x_time_tensor = x_time_tensor.view(-1, sequence_length, x_time_tensor.shape[1])
     concatenated_tensor = concatenated_tensor.view(-1, sequence_length, concatenated_tensor.shape[1])
     y_tensor = y_tensor[sequence_length - 1:]
 
